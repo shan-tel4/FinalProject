249fcacd40e28bb8f2e29affc44922f357c7a060
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2025-07-01 10:57:48 +0100

    Add test for invalid username and invalid password

--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -7,4 +7,9 @@ Feature: Login Functionality
     Then the user should be logged in successfully
 
 
-
+  Scenario: Unsuccessful login with invalid credentials
+    Given the user is on the Swag Lab page
+    When the user enters an invalid username and password
+    And the user clicks the login button
+    Then an error message should be displayed
+    And the user should remain on the login page
--- src/test/scala/stepdefs/LoginSteps.scala
+++ src/test/scala/stepdefs/LoginSteps.scala
@@ -7,8 +7,12 @@ import org.openqa.selenium.chrome.ChromeDriver
 class LoginSteps extends ScalaDsl with EN {
   val driver: WebDriver = new ChromeDriver()
 
+
+  //Validate Successful login with valid username and password
   Given("""the user is on the Swag Lab page""") { () =>
     driver.get("https://www.saucedemo.com/")
+    driver.manage().window().maximize()
+
   }
 
   When("""the user enters valid username and password""") { () =>
@@ -28,5 +32,31 @@ class LoginSteps extends ScalaDsl with EN {
     assert(actualHeader == expectedHeader, s"Expected header '$expectedHeader', but got '$actualHeader'")
 
     driver.quit()
+  }
+
+
+  //Failed Login With Invalid Username and Valid password for Standard User
+  Given("""the user is on the Swag Lab page""") { () =>
+    driver.get("https://www.saucedemo.com/")
+    driver.manage().window().maximize()
+
+  }
+
+  When("""the user enters an invalid username and password""") { () =>
+    driver.findElement(By.cssSelector("#user-name")).sendKeys("Sarah")
+    driver.findElement(By.cssSelector("#password")).sendKeys("Password1234")
+
+  }
+
+  And("""the user clicks the login button""") { () =>
+    driver.findElement(By.cssSelector("#login-button")).click()
+  }
+
+  Then("""an error message should be displayed""") { () =>
+
+    And("""the user should remain on the login page""") { () =>
+
+      driver.quit()
+    }
   }
 }

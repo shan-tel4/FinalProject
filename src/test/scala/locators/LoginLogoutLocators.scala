e9b4367d1e34bf875f64553e280bb18ad274c04a
Connor Archibald <connor.archibald@Connors-MacBook-Air.local>
2025-07-02 16:48:01 +0100

    finishing login/logout tests and product sorting

---
+++ src/test/scala/locators/LoginLogoutLocators.scala
@@ -0,0 +1,13 @@
+package locators
+
+import org.openqa.selenium.By
+
+object LoginLocators {
+
+  // Login Page
+  val usernameField: String = "#user-name"
+  val passwordField: String = "#password"
+  val Submit: String = "input[type='submit']" //login-button
+val errorMessage: String = "div.error-message-container.error > h3"
+
+}
---
+++ src/test/scala/locators/ProductLocators.scala
@@ -0,0 +1,5 @@
+package locators
+
+object ProductLocators {
+
+}

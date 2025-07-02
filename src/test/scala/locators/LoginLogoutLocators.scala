b16cbd582e93780d6399944897d249a2f3b52a9c
Connor Archibald <connor.archibald@Connors-MacBook-Air.local>
2025-07-02 16:48:05 +0100

    finishing login/logout tests and product sorting

--- src/test/scala/locators/LoginLogoutLocators.scala
+++ src/test/scala/locators/LoginLogoutLocators.scala
@@ -2,12 +2,13 @@ package locators
 
 import org.openqa.selenium.By
 
-object LoginLocators {
+object LoginLogoutLocators {
 
-  // Login Page
   val usernameField: String = "#user-name"
   val passwordField: String = "#password"
   val Submit: String = "input[type='submit']" //login-button
 val errorMessage: String = "div.error-message-container.error > h3"
-
+  val menuIcon: String = "react-burger-menu-btn"
+  val logoutLinkField: String = "logout_sidebar_link"
+  val loginLogo: String = "div.login_logo"
 }
--- src/test/scala/locators/ProductLocators.scala
+++ src/test/scala/locators/ProductLocators.scala
@@ -1,5 +1,7 @@
 package locators
 
 object ProductLocators {
-
+  val sortDropdown: String = "select.product_sort_container"
+  val productNameElements: String = "div.inventory_item_name"
+  val productPriceElements: String = "div.inventory_item_price"
 }
